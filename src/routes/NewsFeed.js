06415ea0a8c19ba06d823ca11bc1c9633b605e2d
NekoCientifico <libraaris3.1@gmail.com>
2022-06-07 01:03:00 +0200

    Widget de Twitter integrado y funcional. (Requiere una lista de cuentas que está contenida en la cuenta de Twitter ViGtoryUPC)

--- src/routes/NewsFeed.js
+++ src/routes/NewsFeed.js
@@ -1,6 +1,7 @@
 import React, { useImperativeHandle } from 'react';
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { Follow, Timeline } from 'react-twitter-widgets'
 import {API_address} from '../libraries/API_address';
 //import ReactDOM from 'react-dom';
 import { Routes, Route, Link, useHistory, useNavigate } from "react-router-dom";
@@ -16,7 +17,8 @@ import '../css/NewsFeed.css';
 
 import { Cookie } from '../libraries/cookie';
 import {BaseName} from "../libraries/basename";
-import {getSubjectList} from '../libraries/data_request';
+
+import Twitter_logo from '../assets/images/Twitter_logo.png';
 
 //IMPORTANTE PARA QUE NO SE VEA MAL AL ABRIR EL TECLADO EN MÓVIL
 //https://stackoverflow.com/questions/32963400/android-keyboard-shrinking-the-viewport-and-elements-using-unit-vh-in-css
@@ -40,17 +42,80 @@ class TwitterFeed extends React.Component {
 		this.state = {
 		};
 		
-		
+		this.twitter_name = "ViGtoryUPC"
+		this.lang = "ca";
+		this.theme = "light";//dark
 	}
 
+	/*componentDidMount(){
+		
+	}*/
 
 	render(){
-		
+		let twitter_name = this.twitter_name;
+		let tw_logo_w = 30;
+		let tw_logo_h = tw_logo_w*0.82;
+
 		return(<>
+			<div className="d-flex justify-content-between" >
 
+				<a href={"https://twitter.com/"+twitter_name}>
+					<img
+						className="align-self-end mb-1 ms-2"
+						src={Twitter_logo}
+						width={tw_logo_w.toString()+"px"}
+						height={tw_logo_h.toString()+"px"}
+					/>
+				</a>
 
+				<Follow 
+					username={twitter_name} 
+					options={{ 
+						lang: this.lang,
+						size: "large",
+						count: 'none'
+					}}
+					onLoad={()=>{
+						//console.log("LOADED")
 
+						//let iframe = window.document.getElementById("twitter-widget-0");
+						/*let iframe = window.document.querySelectorAll('[id^="twitter-widget-"]')[0];
+						console.log(iframe);
+						
 
+						let follow_count = iframe.contentWindow.document.querySelector("#count");*/
+
+						//console.log(follow_count);
+					}}
+				/>
+
+			</div>
+
+			<div 
+				className="pb-0"
+				style={{
+					backgroundColor:"white",
+					borderRadius:"1rem",
+					borderStyle:"solid",
+					borderWidth:"0.125rem",
+					borderColor:"rgba(0,127,191,1)",
+					overflow:"hidden"
+				}}
+			>
+				<Timeline
+				style = {{}}
+					dataSource={{
+						sourceType: 'list',
+						ownerScreenName: {twitter_name},
+						id: '1533911007340417027'
+					}}
+					options={{
+						lang: this.lang,
+						theme: this.theme,
+						chrome: "noheader, nofooter"
+					}}
+				/>
+			</div>
 
 		</>);
 	}
@@ -99,9 +164,13 @@ class InitialScreen extends React.Component {
 				<NavBar currentSection={this.props.currentSection} />
 				<br/><br/><br/><br/>
 
-				<div className="grade_calc mx-auto" >
+				<div className="news_feed mx-auto" >
 				
-					<h2 className="text-center mb-4">Notícies:</h2>
+					
+
+					<h2 className="text-center mb-4">
+						Notícies:
+					</h2>
 
 
 					<TwitterFeed key={"twitter_"+(new Date().toString())} />
@@ -109,8 +178,6 @@ class InitialScreen extends React.Component {
 
 
 				</div>
-				<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-				<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 				<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 			</>
 		);
@@ -137,18 +204,23 @@ function NewsFeed(props){
 	function navigateTo(page) {
 		navigate(page);
 	}*/
-	/*useEffect(() => {
-		getSubjectList().then((data) => {
+	let main_ref = React.createRef();
+
+	useEffect(() => {
+		/*getSubjectList().then((data) => {
 			//console.log(data);
 			if (main_ref.current)
 			main_ref.current.updateSubjectList(data);
-		});
-	  }, [window.location.href && new Date()]);*/
+		});*/
+		if (main_ref.current)
+			main_ref.current.forceUpdate();
 
-	//let main_ref = React.createRef();
+		console.log("re-render");
+	  }, [window.location.href && new Date()]);
+
 
 	return(
-		<InitialScreen currentSection={props.currentSection} key={"news_feed"+(new Date().toString())} />
+		<InitialScreen currentSection={props.currentSection} key={"news_feed"+(new Date().toString())} main_ref={main_ref} />
 	)
 }
 export default NewsFeed;
