c51f290e783f162a644bab7fa418cadd7bf5b611
NekoCientifico <libraaris3.1@gmail.com>
2022-06-18 19:45:53 +0200

    Serializada con map la forma en que se renderizan los botones de la barra de navegación superior. Ahora los botones tienen interacción como enlace, por lo que se pueden abrir en pestaña nueva.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,6 +32,26 @@ class InitialScreen extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+
+		this.sectionlist = [
+			{
+				key: "/", 
+				text: "Publicacions"
+			},
+			{
+				key: "/grade_calc", 
+				text: "Calculadora de notes"
+			},
+			{
+				key: "/schedule_gen", 
+				text: "Generador d'horaris"
+			},
+			{
+				key: "/news_feed", 
+				text: "Notícies"
+			},
+		]
 	}
 
 	handleSelect(eventKey){
@@ -59,6 +79,20 @@ class InitialScreen extends React.Component {
 		//let nom_usuari = ((this.userData) ? (this.userData.usuari) : "username");
 		let nom_usuari = Cookie.get("username");
 
+
+
+/*
+<Nav.Link
+	eventKey="/news_feed"
+	className="d-flex justify-content-center align-items-center"
+	as="a"
+	href={(BaseName==="/"?"":BaseName)+"/news_feed"}
+>
+	Notícies
+</Nav.Link>
+*/
+
+
 		return(
 			<>
 			<Navbar className="global_navbar" bg="light" expand="md" fixed="top" style={{zIndex: "99999999"}}>
@@ -69,7 +103,9 @@ class InitialScreen extends React.Component {
 							window.location.protocol+"//"+window.location.host+
 							(BaseName==="/"?"":BaseName);
 						}}>
-						<img id="navbar_title_image" src={logo_ViGtory} className="mx-auto d-block" alt="ViGtory!" />
+						<Link to="/" className="text-decoration-none">
+							<img id="navbar_title_image" src={logo_ViGtory} className="mx-auto d-block" alt="ViGtory!" />
+						</Link>
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="navbarScroll" />
 
@@ -85,27 +121,72 @@ class InitialScreen extends React.Component {
 							navbarScroll 
 							onSelect={(eventKey) => this.props.navigate(eventKey)}
 						>
-
-							<Nav.Link eventKey="/" className="d-flex justify-content-center align-items-center" >Publicacions</Nav.Link>
-
-
-							<Nav.Link eventKey="/grade_calc" className="d-flex justify-content-center align-items-center" >Calculadora de notes</Nav.Link>
-
-
-							<Nav.Link eventKey="/schedule_gen" className="d-flex justify-content-center align-items-center" >Generador d'horaris</Nav.Link>
+{/*
+							<Link to="/" className="text-decoration-none">
+								<Nav.Link eventKey="/" className="d-flex justify-content-center align-items-center" >Publicacions</Nav.Link>
+							</Link>
 
 
-							<Nav.Link eventKey="/news_feed" className="d-flex justify-content-center align-items-center" >Notícies</Nav.Link>
+							<Link to="/grade_calc" className="text-decoration-none">
+								<Nav.Link eventKey="/grade_calc" className="d-flex justify-content-center align-items-center" >Calculadora de notes</Nav.Link>
+							</Link>
 
 
+							<Link to="/schedule_gen" className="text-decoration-none">
+								<Nav.Link eventKey="/schedule_gen" className="d-flex justify-content-center align-items-center" >Generador d'horaris</Nav.Link>
+							</Link>
 
+							<Link to="/news_feed" className="text-decoration-none">
+								<Nav.Link eventKey="/news_feed" className="d-flex justify-content-center align-items-center" >Notícies</Nav.Link>
+							</Link>
+*/}
 
 							
+							{
+								this.sectionlist.map(section => {
+
+								/*return(<>
+									<Nav.Link eventKey={section.key} className="d-flex justify-content-center align-items-center" >
+										{section.text}
+									</Nav.Link>
+								</>);*/
+
+								/*return(<>
+									<Nav.Link eventKey={section.key} className="d-flex justify-content-center align-items-center" >
+										<Link to={section.key} className="text-decoration-none">
+											{section.text}
+										</Link>
+									</Nav.Link>
+									
+								</>);*/
+
+								return(<>
+									<Link to={section.key} className="text-decoration-none">
+										<Nav.Link eventKey={section.key} className="d-flex justify-content-center align-items-center" >
+											{section.text}
+										</Nav.Link>
+									</Link>
+								</>);
+
+								/*return(<>
+									<Nav.Link
+										eventKey={section.key}
+										className="d-flex justify-content-center align-items-center"
+										as="a"
+										href={(BaseName==="/"?"":BaseName)+section.key}
+									>
+										{section.text}
+									</Nav.Link>
+								</>);*/
+							})
+
+							}
+
 
 
 
 						</Nav>
-</div>
+						</div>
 
 
 						<NavDropdown 
@@ -122,16 +203,20 @@ class InitialScreen extends React.Component {
 								}
 						>
 
-								<NavDropdown.Item><Link to={"/user/"+nom_usuari} className="text-reset text-decoration-none d-flex align-items-center">
-									<img src={profile_img} className="user_access_icon d-inline" />
-									El teu perfil
-								</Link></NavDropdown.Item>
+								<Link to={"/user/"+nom_usuari} className="text-reset text-decoration-none d-flex align-items-center">
+									<NavDropdown.Item>
+										<img src={profile_img} className="user_access_icon d-inline" />
+										El teu perfil
+									</NavDropdown.Item>
+								</Link>
 
 
-								<NavDropdown.Item><Link to="/settings/password" className="text-reset text-decoration-none d-flex align-items-center">
-									<img src={settings_img} className="user_access_icon d-inline" />
-									Configuració d'usuari
-								</Link></NavDropdown.Item>
+								<Link to="/settings/password" className="text-reset text-decoration-none d-flex align-items-center">
+									<NavDropdown.Item>
+										<img src={settings_img} className="user_access_icon d-inline" />
+										Configuració d'usuari
+									</NavDropdown.Item>
+								</Link>
 
 								<NavDropdown.Divider />
 
